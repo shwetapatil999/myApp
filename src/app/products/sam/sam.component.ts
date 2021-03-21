1de7ab32d12159e2968333b251d8678743ff8ea1
shwetapatil <shweta.pattil@firstcry.in>
2021-03-21 23:18:18 +0530

    trackBy

--- src/app/products/sam/sam.component.ts
+++ src/app/products/sam/sam.component.ts
@@ -6,33 +6,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sam.component.css']
 })
 export class SamComponent {
-  checkngif: boolean = true;
+  // checkngif: boolean = true;
 
-  isvalid: boolean = true;
-  changeval(valid:any) {
-    this.isvalid = valid;
-  }
+  // isvalid: boolean = true;
+  // changeval(valid:any) {
+  //   this.isvalid = valid;
+  // }
 
-  btnClick() {
-    alert('Dom change behaviour on click')  
+  // btnClick() {
+  //   alert('Dom change behaviour on click')  
+  // }
+
+  // ng for advanced track by
+  students1: any[]; //var declare
+  constructor() {
+    this.students1 = [
+      {
+        'studentId1': 101,
+        'name': 'shweta',
+        'gender': 'female',
+        'qualification': 'BE',
+      },
+      {
+        'studentId1': 102,
+        'name': 'Swara',
+        'gender': 'female',
+        'qualification': 'BE comp'
+      },
+      {
+        'studentId1': 103,
+        'name': 'Pinki',
+        'gender': 'female',
+        'qualification': 'BE It'
+      },
+      {
+        'studentId1': 104,
+        'name': 'Divya',
+        'gender': 'female',
+        'qualification': 'BE ENC'
+      }
+    ];
   }
-  // ng for 
-  students: any[] = [
-    {
-    'name': 'shweta',
-    'qualification' :'BE'
-    },
-    {
-      'name': 'Swara',
-      'qualification': 'BE comp'
-    },
-    {
-      'name': 'Pinki',
-      'qualification': 'BE It'
-    },
-    {
-      'name': 'Divya',
-      'qualification': 'BE ENC'
-    }
-  ]
+  getmoreStudents(): void {
+    this.students1 = [
+      {
+        'studentId1': 101,
+        'name': 'shweta',
+        'gender': 'female',
+        'qualification': 'BE',
+      },
+      {
+        'studentId1': 102,
+        'name': 'Swara',
+        'gender': 'female',
+        'qualification': 'BE comp'
+      },
+      {
+        'studentId1': 103,
+        'name': 'Pinki',
+        'gender': 'female',
+        'qualification': 'BE It'
+      },
+      {
+        'studentId1': 104,
+        'name': 'Divya',
+        'gender': 'female',
+        'qualification': 'BE ENC'
+      },
+      {
+        'studentId1': 105,
+        'name': 'Divya',
+        'gender': 'female',
+        'qualification': 'BE ENC'
+      }
+    ];
+  }
+  trackbystudentId(index: number, students1: any): string{
+    return students1.studentId1;
+  }
 }
